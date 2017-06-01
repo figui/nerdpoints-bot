7c6eb06cb98d757aa5f64d48ed2b77524f712aa6
Fernando Iguibejeres <fernando.iguibejeres@mercadolibre.com>
2017-06-01 00:09:10 -0300

    Add last month score command

--- nerdpoint-telegram.js
+++ nerdpoint-telegram.js
@@ -105,6 +105,6 @@ bot.onText(/\/current/, (msg, match) => {
         })
 });
 
-bot.onText(/\/lastMonth/, (msg, match) => {
+bot.onText(/\/lastmonth/, (msg, match) => {
     bot.sendMessage(msg.chat.id, nerdpoints.lastMonth(), {parse_mode : "Markdown"});
 });
